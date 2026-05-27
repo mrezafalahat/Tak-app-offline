277c7fc5b57a496637cae20192221def11efe9fa
mrezafalahat <121670374+mrezafalahat@users.noreply.github.com>
2026-05-27 14:11:02 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,5 @@
-const CACHE_NAME = 'tak-ui-v1';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+const CACHE_NAME = 'tak-ui-v2';
+const APP_SHELL = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
